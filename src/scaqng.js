4aa1b7059e14578a59dfe3c36516691efa2be0e0
Astavie <astavie@protonmail.com>
2020-10-23 11:43:34 +0200

    more numbers

--- src/scaqng.js
+++ src/scaqng.js
@@ -1,9 +1,13 @@
 var numerals = [
-    {dec: 36, num: "f"},
-    {dec: 18, num: "n"},
-    {dec: 6, num: "r"},
-    {dec: 3, num: "m"},
-    {dec: 1, num: "|"}
+    {dec: 1296, num: "ž"}, // Žnočún
+    {dec: 648, num: "p"}, // variation on uM
+    {dec: 216, num: "š"}, // Šun
+    {dec: 108, num: "s"}, // variation on uM
+    {dec: 36, num: "f"}, // Fež
+    {dec: 18, num: "n"}, // variation on uM
+    {dec: 6, num: "r"}, //  Jeqàg (used to be an R in the past)
+    {dec: 3, num: "m"}, // uM
+    {dec: 1, num: "|"} // Looks like 1
 ]
 
 function convert() {
